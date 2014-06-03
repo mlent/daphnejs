b95a6b07baa112f1117f5c61705af9e3b4adc041
Monica <lent.monica@gmail.com>
2014-06-03 09:30:16 -0500

    Basic tree display works.

--- daphne.js
+++ daphne.js
@@ -2,13 +2,13 @@
 !function(root, factory) {
 
 	if (typeof define === 'function' && define.amd) {
-		define(['jquery'], factory);
+		define(['jquery', 'd3'], factory);
 	}
 	else {
-		factory(jQuery);
+		factory(jQuery, d3);
 	}
 
-}(this, function($) {
+}(this, function($, d3) {
 	'use strict';
 
 	// Plugin Constructor
@@ -23,7 +23,19 @@
 
 		defaults: {
 			'mode': 'edit',
-			'source': 'data.json' 	// For testing purposes, will be removed
+			'source': 'data.json', 	// For testing purposes, will be removed
+			'dimensions': {
+				'margins': {
+					'top': 50,
+					'right': 0,
+					'bottom': 50,
+					'left': 0
+				},
+				'width': 600,
+				'height': 600,
+				'initialScale': 0.9
+			},
+			'duration': 500
 		},
 
 		init: function() {
@@ -81,9 +93,171 @@
 		 * Uses the fetched data to render the parse tree, considering user options
 		 */
 		render: function(e) {
-			console.log(this.data);
-		}
+			
+			var that = this;
 
+			// To keep multiple instances from stomping on each other's data/d3 references
+			this.tree = d3.layout.tree().nodeSize([100, 50]);
+
+			this.tree.separation(function(a, b) {
+				var w1 = (a.value.length > a.relation.length) ? a.value.length : a.relation.length;
+				var w2 = (b.value.length > b.relation.length) ? b.value.length : b.relation.length;
+
+				var  scale = 0.13;
+
+				return Math.ceil((w1 * scale) + (w2 * scale) / 2);
+			});
+
+			var diagonal = d3.svg.diagonal().projection(function(d) {
+				return [d.x, d.y];
+			});
+
+			// TODO: Get tag names from conf file
+			this.color = d3.scale.ordinal()
+				.domain(["noun", "verb", "participle", "adj", "adverb", "particle", "conj", "prep", "pron", "numeral", "interjection", "exclam", "punct", "article", "root", "_", "unassigned"])
+				.range(["#019292", "#D15241", "#8CD141", "#4E6087", "#8CD141", "#FF881A", "#754574", "#149069", "#523D5B", "#812F0F", "#F4BC78", "#F4BC78", "#1D78AA", "#257008", "#333", "#999", "#999"]);
+
+			// Now we start working on the elements themselves
+			this.svg = d3.select(this.el).append('svg')
+				.attr('class', 'svg-container');
+			this.canvas = this.svg.append('g')
+				.attr('class', 'canvas');
+			this.canvas.append('g')
+				.attr('transform', 'translate(' + 
+					((that.config.dimensions.width / 2)) + ', ' + 
+					that.config.dimensions.margins.top + ') scale(' + 
+					that.config.dimensions.initialScale + 
+				')');
+
+			// Zooming and scale function for SVG -- attached to action object after update function below
+			function zoom() {
+				var scale = d3.event.scale,
+					translation = d3.event.translate,
+					tbound = -that.config.dimensions.height * scale,
+					bbound = that.config.dimensions.height * scale,
+					lbound = (-that.config.dimensions.width + that.config.dimensions.margins.right) * scale,
+					rbound = (that.config.dimensions.width + that.config.dimensions.margins.left) * scale;
+
+				var translation = [
+					Math.max(Math.min(translation[0], rbound), lbound),
+					Math.max(Math.min(translation[1], bbound), tbound)
+				];
+
+				that.canvas.attr('transform', 'translate(' + translation + ') scale(' + scale + ')');
+			}
+
+			// And alas, the d3 update function
+			this.root = this.data[0];
+			update(this.root);
+
+			/**
+			 * Update function is called every time the structure of the tree changes. It manages adjusting
+			 * the positions of the nodes, their colors, displaying attirbutes and relations, etc.
+			 *
+			 */
+			function update(source) {
+				var nodes = that.tree(that.root).reverse(),
+					links = that.tree.links(nodes);
+
+				nodes.forEach(function(d) {
+					d.y = d.depth * 100;
+				});
+
+				var node = that.svg.select('.canvas g').selectAll('g.node')
+					.data(nodes, function(d) {
+						return d.id || (d.id = ++i);
+					});
+
+				var nodeEnter = node.enter().append('g')
+					.attr('class', 'node')
+					.attr('transform', function(d) {
+						return 'translate(' + source.x + ', ' + source.y + ')';
+					});
+
+				nodeEnter.append('circle')
+					.attr('r', 10)
+					.style('stroke', function(d) {
+						return that.color(d.pos);
+					})
+					.style('fill', function(d) {
+						return '#FFF';
+					});
+
+				nodeEnter.append('text')
+					.attr('y', function(d, i) {
+						return (d.pos == 'root') ? -30 : 15;
+					})
+					.attr('dy', '14px')
+					.attr('text-anchor', 'middle')
+					.text(function(d) {
+						return d.value;
+					})
+					.style('fill', function(d, i) {
+						return (d.pos == 'root') ? '#CCC' : '#333';
+					})
+					.style('fill-opacity', 1);
+
+				nodeEnter.append('text')
+					.attr('y', function(d, i) {
+						return (d.pos == 'root') ? 0 : -30;
+					})
+					.attr('dy', '12px')
+					.attr('text-anchor', 'middle')
+					.attr('class', 'label')
+					.text(function(d) {
+						return d.relation;
+					});
+
+				var nodeUpdate = node.transition()
+					.duration(that.config.duration)
+					.attr('transform', function(d) {
+						return 'translate(' + d.x + ', ' + d.y + ')';
+					});
+
+				nodeUpdate.select('text.label')
+					.text(function(d) {
+						return d.relation;
+					});
+
+				nodeUpdate.select('circle')
+					.style('stroke', function(d) {
+						return that.color(d.pos);
+					})
+					.style('fill', function(d) {
+						return '#FFF';
+					});
+
+				var link = that.svg.select('.canvas g').selectAll('path.link')
+					.data(links, function(d) {
+						return d.target.id;
+					});
+
+				link.enter().insert('path', 'g')
+					.attr('class', 'link')
+					.attr('d', function(d) {
+						var o = { x: source.x, y: source.y };
+						return diagonal({ source: o, target: o });
+					});
+
+				link.transition()
+					.duration(that.config.duration)
+					.attr('d', diagonal);
+
+				nodes.forEach(function(d, i) {
+					d.x0 = d.x;
+					d.y0 = d.y;
+				});
+			}
+			// End of Update function
+
+			d3.select(this.$el.find('svg')[0])
+				.call(d3.behavior.zoom()
+					.scaleExtent([0.5, 5])
+					.on("zoom", zoom))
+				.on('dblclick.zoom', null);
+
+			this.update = update;
+		}
 	};
 
 	$.fn.daphne = function(options) {
--- demo/main.js
+++ demo/main.js
@@ -2,16 +2,20 @@ requirejs.config({
 	'baseUrl': '/daphne/demo',
 	'paths': {
 		'jquery': '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min',
-		'd3': '//cdnjs.cloudflare.com/ajax/libs/d3/3.4.8/d3.min'
+		'd3': '//cdnjs.cloudflare.com/ajax/libs/d3/3.4.8/d3.min',
+		'daphne': '../daphne'
 	},
 	'shim': {
 		'd3': {
 			'exports': 'd3'
+		},
+		'daphne': {
+			'deps': ['jquery', 'd3']
 		}
 	}
 });
 
-require(['jquery', 'd3', '../daphne'], function($, d3, daphne) {
+require(['jquery', 'daphne'], function($, daphne) {
 	$(function() {
 		$('div[data-toggle="daphne"]').daphne();
 	});
