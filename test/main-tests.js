f6416dde701596ea3008b4778c33ac79c5e91498
Monica <lent.monica@gmail.com>
2014-06-15 11:49:49 -0500

    Installed grunt, updated package.json, updated bower.json, started structure for running mocha+require+phantomjs tests.

---
+++ test/main-tests.js
@@ -0,0 +1,18 @@
+require.config({
+	baseUrl: '.',
+	paths: {
+		'd3': '//cdnjs.cloudflare.com/ajax/libs/d3/3.4.8/d3.min',
+		'bower_components/chai/chai'
+	}
+});
+
+require(['chai'], function(chai) {
+	chai.should();
+	window.expect = chai.expect;
+	mocha.setup("tdd");
+
+	require(['specs'], function() {
+		mocha.run();
+	});
+});
+
