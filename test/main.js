e8d85e159d03073a14d1cecfb7dbcdd2a30057c7
Monica <lent.monica@gmail.com>
2014-06-16 06:25:13 -0500

    Mocha + Blanket tests running successfully in browser.

--- test/main.js
+++ test/main.js
@@ -2,16 +2,17 @@ require.config({
 	baseUrl: '../',
 	paths: {
 		'd3': '//cdnjs.cloudflare.com/ajax/libs/d3/3.4.8/d3.min',
-		'bower_components/chai/chai'
+		'chai': 'bower_components/chai/chai',
 	}
 });
 
 require(['chai'], function(chai) {
 	chai.should();
 	window.expect = chai.expect;
+	window.assert = chai.assert;
 	mocha.setup("tdd");
 
-	require(['specs'], function() {
+	require(['test/specs'], function() {
 		mocha.run();
 	});
 });
